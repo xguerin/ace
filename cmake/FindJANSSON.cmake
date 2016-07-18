6c984daaaa9135b0e8c6f0d549b4e93f227465d3
Xavier R. Guérin <xguerin@users.noreply.github.com>
2016-07-17 23:52:12 -0400

    Improve CMake macros and build files

--- cmake/FindJANSSON.cmake
+++ cmake/FindJANSSON.cmake
@@ -1,32 +1,33 @@
 # - Find JANSSON 
 # Find the native JANSSON includes and library
 #
-#  JANSSON_INCLUDE_DIR - where to find re2.h, etc.
-#  JANSSON_LIBRARIES   - List of libraries when using JANSSON.
-#  JANSSON_FOUND       - True if JANSSON found.
+#  JANSSON_INCLUDE_DIRS - where to find re2.h, etc.
+#  JANSSON_LIBRARIES    - List of libraries when using JANSSON.
+#  JANSSON_FOUND        - True if JANSSON found.
 
-find_path(JANSSON_INCLUDE_DIR NAMES jansson.h PATHS
-  /usr/include
-  /usr/local/include
-  )
-
-find_library(JANSSON_LIBRARY NAMES jansson PATHS
-  /usr/lib
-  /usr/local/lib
-  )
-
-if (JANSSON_INCLUDE_DIR AND JANSSON_LIBRARY)
+if (JANSSON_INCLUDE_DIRS AND JANSSON_LIBRARIES)
   set(JANSSON_FOUND TRUE)
-  set(JANSSON_LIBRARIES ${JANSSON_LIBRARY} )
 else ()
-  set(JANSSON_FOUND FALSE)
-  set( JANSSON_LIBRARIES )
+  find_path(JANSSON_INCLUDE_DIR NAMES jansson.h PATHS
+    /usr/include
+    /usr/local/include)
+  find_library(JANSSON_LIBRARY NAMES jansson PATHS
+    /usr/lib
+    /usr/local/lib)
+  if (JANSSON_INCLUDE_DIR AND JANSSON_LIBRARY)
+    set(JANSSON_FOUND TRUE)
+    set(JANSSON_INCLUDE_DIRS ${JANSSON_INCLUDE_DIR})
+    set(JANSSON_LIBRARIES ${JANSSON_LIBRARY})
+  else ()
+    set(JANSSON_FOUND FALSE)
+    set(JANSSON_LIBRARIES)
+  endif ()
 endif ()
 
 if (JANSSON_FOUND)
-  message(STATUS "Found JANSSON: ${JANSSON_LIBRARY}")
+  message(STATUS "Found JANSSON: ${JANSSON_LIBRARIES}")
 else ()
-  message(STATUS "Not Found JANSSON: ${JANSSON_LIBRARY}")
+  message(STATUS "Not Found JANSSON: ${JANSSON_LIBRARIES}")
   message(FATAL_ERROR "Could NOT find JANSSON library")
 endif ()
 
--- cmake/FindRE2.cmake
+++ cmake/FindRE2.cmake
@@ -1,40 +1,34 @@
 # - Find RE2 
 # Find the native RE2 includes and library
 #
-#  RE2_INCLUDE_DIR - where to find re2.h, etc.
-#  RE2_LIBRARIES   - List of libraries when using RE2.
-#  RE2_FOUND       - True if RE2 found.
+#  RE2_INCLUDE_DIRS - where to find re2.h, etc.
+#  RE2_LIBRARIES    - List of libraries when using RE2.
+#  RE2_FOUND        - True if RE2 found.
 
-find_path(RE2_INCLUDE_DIR re2/re2.h NO_DEFAULT_PATH PATHS
-  /usr/include
-  /opt/local/include
-  /usr/local/include
-)
-
-set(RE2_NAMES ${RE2_NAMES} re2)
-find_library(RE2_LIBRARY NAMES ${RE2_NAMES} NO_DEFAULT_PATH PATHS
-    /usr/local/lib
-    /usr/local/re2/lib
-    /opt/local/lib
-    /usr/lib
-    )
-
-if (RE2_INCLUDE_DIR AND RE2_LIBRARY)
+if (RE2_INCLUDE_DIRS AND RE2_LIBRARIES)
   set(RE2_FOUND TRUE)
-  set( RE2_LIBRARIES ${RE2_LIBRARY} )
 else ()
-  set(RE2_FOUND FALSE)
-  set( RE2_LIBRARIES )
+  find_path(RE2_INCLUDE_DIR re2/re2.h NO_DEFAULT_PATH PATHS
+    /usr/include
+    /usr/local/include)
+  find_library(RE2_LIBRARY NAMES re2 NO_DEFAULT_PATH PATHS
+    /usr/local/lib
+    /usr/lib)
+  if (RE2_INCLUDE_DIR AND RE2_LIBRARY)
+    set(RE2_FOUND TRUE)
+    set(RE2_INCLUDE_DIRS ${RE2_INCLUDE_DIR})
+    set(RE2_LIBRARIES ${RE2_LIBRARY})
+  else ()
+    set(RE2_FOUND FALSE)
+    set(RE2_LIBRARIES)
+  endif ()
 endif ()
 
 if (RE2_FOUND)
-  message(STATUS "Found RE2: ${RE2_LIBRARY}")
+  message(STATUS "Found RE2: ${RE2_LIBRARIES}")
 else ()
-  message(STATUS "Not Found RE2: ${RE2_LIBRARY}")
+  message(STATUS "Not Found RE2: ${RE2_LIBRARIES}")
   message(FATAL_ERROR "Could NOT find RE2 library")
 endif ()
 
-mark_as_advanced(
-  RE2_LIBRARY
-  RE2_INCLUDE_DIR
-  )
+mark_as_advanced(RE2_LIBRARIES RE2_INCLUDE_DIRS)
